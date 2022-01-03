d4a6e04bf14f6fd6731c06a80707456f8c338ad4
TendTo <casablancaernesto@gmail.com>
2022-01-03 21:00:51 +0100

    bump: ytdl_core 0.0.1 -> 0.0.2

--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ export { Encoder } from "https://unpkg.com/@evan/wasm@0.0.63/target/opus/deno.js
 export { secretbox } from "https://unpkg.com/@evan/wasm@0.0.63/target/nacl/deno.js";
 export { readableStreamFromIterable } from "https://deno.land/std@0.98.0/io/streams.ts";
 export { YouTube } from "https://deno.land/x/youtube_sr/mod.ts";
-export { getInfo } from "https://deno.land/x/ytdl_core@0.0.1/mod.ts";
+export { getInfo } from "https://deno.land/x/ytdl_core@v0.0.2/mod.ts";
 export { readableStreamFromReader } from "https://deno.land/std@0.98.0/io/streams.ts";
